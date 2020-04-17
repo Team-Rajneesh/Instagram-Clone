afdea7687d55b654bc2ccc4c60144e9bbe6a1485
Amit Kumar Sahoo <amit.sahoo@mindfiresolutions.com>
2020-04-17 14:53:43 +0530

    - Add Icons - Add Bottom nav menus

--- app/src/main/java/com/rajneesh/instagramclone/MainActivity.kt
+++ app/src/main/java/com/rajneesh/instagramclone/MainActivity.kt
@@ -5,21 +5,36 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 
+/**
+ * Created by - Rajneesh Shukla
+ * Created on - 17/04/2020
+ * Used For -
+ */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textMessage: TextView
+
+    // Handle Bottom nav tab switching
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
                 textMessage.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+            R.id.navigation_search -> {
+                textMessage.setText(R.string.title_search)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+            R.id.navigation_add -> {
+                textMessage.setText(R.string.title_add)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_heart -> {
+                textMessage.setText(R.string.title_heart)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+                textMessage.setText(R.string.title_profile)
                 return@OnNavigationItemSelectedListener true
             }
         }
