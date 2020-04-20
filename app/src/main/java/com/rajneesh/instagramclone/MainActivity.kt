a261d5544c43ff0319904456e5c2a5744737f94d
Amit Kumar Sahoo <amit.sahoo@mindfiresolutions.com>
2020-04-20 13:10:49 +0530

    - Add Home, Search, Heart, profile and add Fragment - Handle fragment transaction on bottom nav tab click

--- app/src/main/java/com/rajneesh/instagramclone/MainActivity.kt
+++ app/src/main/java/com/rajneesh/instagramclone/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.rajneesh.instagramclone.fragments.*
 
 /**
  * Created by - Rajneesh Shukla
@@ -12,32 +14,30 @@ import android.widget.TextView
  */
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
+    internal var mSelectedFragment:Fragment? = null
 
     // Handle Bottom nav tab switching
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
+               mSelectedFragment = HomeFragment()
             }
             R.id.navigation_search -> {
-                textMessage.setText(R.string.title_search)
-                return@OnNavigationItemSelectedListener true
+                mSelectedFragment = SearchFragment()
             }
             R.id.navigation_add -> {
-                textMessage.setText(R.string.title_add)
-                return@OnNavigationItemSelectedListener true
+                mSelectedFragment = AddFragment()
             }
             R.id.navigation_heart -> {
-                textMessage.setText(R.string.title_heart)
-                return@OnNavigationItemSelectedListener true
+                mSelectedFragment = NotificationsFragment()
             }
             R.id.navigation_profile -> {
-                textMessage.setText(R.string.title_profile)
-                return@OnNavigationItemSelectedListener true
+                mSelectedFragment = ProfileFragment()
             }
         }
+
+        mSelectedFragment?.let { replaceFragmentInContainer(it) }
+
         false
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,7 +45,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
+        // Default selected fragment
+        replaceFragmentInContainer(HomeFragment())
+
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    /**
+     * Replace fragment in Home screen
+     */
+    fun replaceFragmentInContainer(fragment:Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
     }
 }
