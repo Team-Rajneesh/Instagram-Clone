87129231cd5e81632d79fa9210e0fb7b50c826c4
Amit Kumar Sahoo <amit.sahoo@mindfiresolutions.com>
2020-05-02 23:37:38 +0530

    - Handle click event on button

--- app/src/main/java/com/rajneesh/instagramclone/activities/login/views/SignUpActivity.kt
+++ app/src/main/java/com/rajneesh/instagramclone/activities/login/views/SignUpActivity.kt
@@ -2,12 +2,30 @@ package com.rajneesh.instagramclone.activities.login.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.rajneesh.instagramclone.R
+import com.rajneesh.instagramclone.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mBinding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+
+        mBinding.registerBtn.setOnClickListener(this)
+        mBinding.loginScreenBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.register_btn ->{
+                //Todo:  Register user
+            }
+            R.id.login_screen_btn -> {
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/com/rajneesh/instagramclone/activities/login/views/SingInActivity.kt
+++ app/src/main/java/com/rajneesh/instagramclone/activities/login/views/SingInActivity.kt
@@ -1,13 +1,33 @@
 package com.rajneesh.instagramclone.activities.login.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.rajneesh.instagramclone.R
+import com.rajneesh.instagramclone.databinding.ActivitySingnInBinding
 
-class SingInActivity : AppCompatActivity() {
+class SingInActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBinding: ActivitySingnInBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_singn_in)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_singn_in)
+
+        mBinding.loginBtn.setOnClickListener(this)
+        mBinding.signUpTv.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.login_btn ->{
+             //Todo:: Login user
+            }
+            R.id.sign_up_tv -> {
+                startActivity(Intent(this, SignUpActivity::class.java))
+            }
+        }
     }
 }
