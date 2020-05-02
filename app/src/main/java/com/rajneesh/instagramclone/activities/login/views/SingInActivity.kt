7c174e7f927213fe9f49cba183765fe1260c54d1
Amit Kumar Sahoo <amit.sahoo@mindfiresolutions.com>
2020-05-02 21:08:38 +0530

    - Add UI for Login activity

---
+++ app/src/main/java/com/rajneesh/instagramclone/activities/login/views/SingInActivity.kt
@@ -0,0 +1,13 @@
+package com.rajneesh.instagramclone.activities.login.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rajneesh.instagramclone.R
+
+class SingInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singn_in)
+    }
+}
