e03bda129044d92690aaeff700eb98d588209e34
Amit Kumar Sahoo <amit.sahoo@mindfiresolutions.com>
2020-05-02 22:42:06 +0530

    - Add UI for SignUp activity

---
+++ app/src/main/java/com/rajneesh/instagramclone/activities/login/views/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.rajneesh.instagramclone.activities.login.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rajneesh.instagramclone.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
